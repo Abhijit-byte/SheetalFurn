033ef676e481b0e896ec68ce8e545334763c9351
abhijitdash5370 <abhijitdash5370@gmail.com>
2026-07-22 21:17:55 +0530

    Add color variant images for all finishes of Cafe 7 table

--- product-data.js
+++ product-data.js
@@ -799,17 +799,17 @@ const productData = {
         {
           "name": "Gravel - 210",
           "color": "#A3A3A3",
-          "image": "products/Woodwork/Cafe tables/Cafe 7/IMG_4035.jpeg"
+          "image": "products/Woodwork/Cafe tables/Cafe 7/gravel.png"
         },
         {
           "name": "Midnight - 120",
           "color": "#2D3E50",
-          "image": "products/Woodwork/Cafe tables/Cafe 7/IMG_4035.jpeg"
+          "image": "products/Woodwork/Cafe tables/Cafe 7/Midnight - 120.png"
         },
         {
           "name": "Crimson - 808",
           "color": "#8A2B35",
-          "image": "products/Woodwork/Cafe tables/Cafe 7/IMG_4035.jpeg"
+          "image": "products/Woodwork/Cafe tables/Cafe 7/Crimson - 808.png"
         }
       ]
     },
