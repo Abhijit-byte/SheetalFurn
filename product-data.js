54b898e8516f6a6984eaeecbbc4b50a44baec304
KRITI SREYASH PARIDA <kspchimun@gmail.com>
2026-07-22 18:26:53 +0530

    Keep original gallery photos in top scroller for Comfy hb; color photos used in configurator section only

--- product-data.js
+++ product-data.js
@@ -2069,7 +2069,6 @@ const productData = {
     "comfy-hb": {
       "name": "Comfy hb",
       "images": [
-        "products/Seating/Executive Chairs/Comfy hb/comfy-black.png",
         "products/Seating/Executive Chairs/Comfy hb/IMG_3248.jpeg",
         "products/Seating/Executive Chairs/Comfy hb/IMG_3249.jpeg",
         "products/Seating/Executive Chairs/Comfy hb/IMG_3250.jpeg",
